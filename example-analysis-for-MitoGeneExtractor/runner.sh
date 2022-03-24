826169721de9b34895f1b35b6dbd07ac37e72889
Christoph Mayer <c.mayer@leibniz-zfmk.de>
2022-03-24 01:23:50 +0100

    example-analysis-MGE

---
+++ example-analysis-for-MitoGeneExtractor/runner.sh
@@ -0,0 +1,2 @@
+/Daten/Programmieren/C++/Programme/align-trim-DNA-against-Protein/MitoGeneExtractor-v1.9.1-dist/MitoGeneExtractor-v1.9.1 -d SRR12554985_trimmed_reduced.fas -p ../Amino-Acide-references-for-tanomic-groups/COI-vertebrata-protein-consensus-50_whole.fasta -V vulgar-SRR12554985.txt -o SRR12554985_align.fas -n 0 -c SRR12554985_cons.fas -t 0.5 -r 1 -C 2
+
