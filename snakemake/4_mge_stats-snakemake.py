c725e955a750ba0af9153503b6d7536dc5d6f14f
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2024-08-28 11:15:42 +0100

    Update 4_mge_stats-snakemake.py

--- snakemake/4_mge_stats-snakemake.py
+++ snakemake/4_mge_stats-snakemake.py
@@ -1,9 +1,8 @@
 import os
 import sys
 import pandas as pd
-import time
-from Bio.Blast import NCBIWWW, NCBIXML
 import numpy as np
+
 
 
 def extract_data_from_out_files(folder_path, output_csv):
@@ -83,6 +82,8 @@ def extract_data_from_out_files(folder_path, output_csv):
         print(f"Folder '{folder_path}' not found.")
 
 
+
+
 def process_fasta_file(fasta_file):
     sequences = {}
     try:
@@ -108,12 +109,14 @@ def process_fasta_file(fasta_file):
     return sequences
 
 
+
+
+
 def add_fasta_info_to_df(df, fasta_sequences, consensus_column):
     lengths = []
     non_gtac_counts = []
     n_counts = []
     dash_tilde_counts = []
-    blast_results = []
 
     for _, row in df.iterrows():
         consensus_id = row.get(consensus_column, "")
@@ -125,134 +128,43 @@ def add_fasta_info_to_df(df, fasta_sequences, consensus_column):
             non_gtac_count = 0
             n_count = 0
             dash_tilde_count = 0
-            blast_result = {
-                'Title': np.nan,
-                'Score': np.nan,
-                'Alignment Length': np.nan,
-                'Identity': np.nan,
-                'Percent Identity': np.nan,
-                'Gaps': np.nan,
-                'Query Coverage': np.nan,
-                'Subject Coverage': np.nan,
-                'Accession': np.nan
-            }
         else:
             sequence_length = sum(1 for char in sequence if char in 'GTAC')
             non_gtac_count = sum(1 for char in sequence if char not in 'GTAC')
             n_count = sequence.count('N')
             dash_tilde_count = sequence.count('-') + sequence.count('~')
-            blast_result = run_blast(sequence)
 
         lengths.append(sequence_length)
         non_gtac_counts.append(non_gtac_count)
         n_counts.append(n_count)
         dash_tilde_counts.append(dash_tilde_count)
-        blast_results.append(blast_result)
 
     df["Sequence Length"] = lengths
     df["N Count"] = n_counts
     df["Dash and Tilde Count"] = dash_tilde_counts
     df["Non-GTAC Count"] = non_gtac_counts
-    df["BLAST Title"] = [result.get('Title', np.nan) for result in blast_results]
-    df["BLAST Score"] = [result.get('Score', np.nan) for result in blast_results]
-    df["BLAST Alignment Length"] = [result.get('Alignment Length', np.nan) for result in blast_results]
-    df["BLAST Identity"] = [result.get('Identity', np.nan) for result in blast_results]
-    df["BLAST Percent Identity"] = [result.get('Percent Identity', np.nan) for result in blast_results]
-    df["BLAST Gaps"] = [result.get('Gaps', np.nan) for result in blast_results]
-    df["BLAST Query Coverage"] = [result.get('Query Coverage', np.nan) for result in blast_results]
-    df["BLAST Subject Coverage"] = [result.get('Subject Coverage', np.nan) for result in blast_results]
-    df["BLAST Accession"] = [result.get('Accession', np.nan) for result in blast_results]
 
     return df
 
 
-def run_blast(sequence):
-    if not sequence or sequence.strip() == "":
-        print("Error: Attempting to run BLAST with an empty sequence.")
-        return {
-            'Title': np.nan,
-            'Score': np.nan,
-            'Alignment Length': np.nan,
-            'Identity': np.nan,
-            'Percent Identity': np.nan,
-            'Gaps': np.nan,
-            'Query Coverage': np.nan,
-            'Subject Coverage': np.nan,
-            'Accession': np.nan
-        }
-
-    try:
-        result_handle = NCBIWWW.qblast("blastn", "nt", sequence)
-        blast_records = NCBIXML.parse(result_handle)
-
-        for blast_record in blast_records:
-            for alignment in blast_record.alignments:
-                for hsp in alignment.hsps:
-                    percent_identity = (hsp.identities / hsp.align_length) * 100
-                    return {
-                        'Title': alignment.title,
-                        'Score': hsp.score,
-                        'Alignment Length': hsp.align_length,
-                        'Identity': hsp.identities,
-                        'Percent Identity': percent_identity,
-                        'Gaps': hsp.gaps,
-                        'Query Coverage': f"{hsp.query_start}-{hsp.query_end}",
-                        'Subject Coverage': f"{hsp.sbjct_start}-{hsp.sbjct_end}",
-                        'Accession': alignment.accession
-                    }
-
-        return {
-            'Title': np.nan,
-            'Score': np.nan,
-            'Alignment Length': np.nan,
-            'Identity': np.nan,
-            'Percent Identity': np.nan,
-            'Gaps': np.nan,
-            'Query Coverage': np.nan,
-            'Subject Coverage': np.nan,
-            'Accession': np.nan
-        }
-
-    except Exception as e:
-        print(f"Error running BLAST: {e}")
-        return {
-            'Title': np.nan,
-            'Score': np.nan,
-            'Alignment Length': np.nan,
-            'Identity': np.nan,
-            'Percent Identity': np.nan,
-            'Gaps': np.nan,
-            'Query Coverage': np.nan,
-            'Subject Coverage': np.nan,
-            'Accession': np.nan
-        }
-
-    finally:
-#Sleep to handle rate limiting
-        time.sleep(2)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("""
-        Usage: python 5_mge_stats.py /path/to/.out /path/to/[summary_stats].csv /path/to/multi.fasta
+        Usage: python 5_mge_stats.py /path/to/.out /path/to/[summary_stats].csv /path/to/multi-fasta
 
-        Extract data from MGE .out files, MGE concatenated cox1 consensus sequences in multi-FASTA file, and BLAST results to the output .csv
+        Extract data from MGE .out files and MGE concatenated cox1 consensus sequences in multi-FASTA file to the output .csv
         
         MGE output:
         Length of alignment = length of subject/reference sequence 
         Sequence found in vulgar file = number of sequences used by exonerate
         # skipped reads due to low rel. score = number of reads omitted by MGE due to a low relative exonerate alignment score (-r)
-
-        BLAST output:
-        Title = title of BLAST alignment
-        Score = HSP score assigned to the alignment (higher = better)
-        Expect = E-value (expectation value), which indicates the likelihood of the match occurring by chance (lower = better)
-        Identity = number of identical matches between the query and subject sequences
-        Query Coverage = range of positions in query (input) sequence covered by alignment
-        Subject Coverage = range of positions in the subject sequence covered by alignment
         """)
         sys.exit(1)
+
+
+
 
     folder_path = sys.argv[1]
     output_csv = sys.argv[2]
@@ -268,6 +180,6 @@ if __name__ == "__main__":
         summary_df = add_fasta_info_to_df(summary_df, fasta_sequences, "consensus sequence output")
 
         summary_df.to_csv(output_csv, index=False)
-        print(f"Updated summary stats saved to {output_csv}")
+        print(f"Summary stats saved to {output_csv}")
     else:
-        print(f"Error: No valid data extracted. The output CSV '{output_csv}' is empty or does not exist.")
+        print(f"Error: No data extracted. The output CSV '{output_csv}' is empty or does not exist.")
