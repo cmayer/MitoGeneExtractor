bd62d0fe3da4575eb86faffe646624e82a0891b1
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2024-09-30 15:17:37 +0100

    Update snakemake.sh

--- snakemake/snakemake.sh
+++ snakemake/snakemake.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=MGE-test
-#SBATCH --partition=hour
+#SBATCH --job-name=MGE
+#SBATCH --partition=day
 #SBATCH --output=%x.out
 #SBATCH --mem-per-cpu=10G
 #SBATCH --cpus-per-task=16
 #SBATCH --error=%x.err
 
-# Activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda init bash
 conda activate mge_env
 
+
 snakemake \
    --snakefile Snakefile \
+   --configfile config.yaml \
    --cores 16
