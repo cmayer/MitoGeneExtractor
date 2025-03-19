84e2e63bfe275892104f7278d69fb3eeb48e4039
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2025-03-19 17:17:23 +0000

    Update snakemake.sh

--- snakemake/snakemake.sh
+++ snakemake/snakemake.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=MGE
-#SBATCH --partition=day
-#SBATCH --output=%x.out
-#SBATCH --mem=250G
-#SBATCH --cpus-per-task=16
-#SBATCH --error=%x.err
+##These SBATCH parameters (parition, mem, and cpus-per-task) are solely for the main snakemake process.
+##These may need some tweaking, although 5-20G of memory and 1-4 CPUs is likely more than enough.
+#SBATCH --job-name=mge
+#SBATCH --partition=long
+#SBATCH --mem=15G
+#SBATCH --cpus-per-task=4
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda init bash
+
+
+## Conda environment
+
+# Set path to conda explicitly
+export PATH="/path/to/conda/bin:$PATH"
+
+# Source conda.sh
+source /path/to/conda/etc/profile.d/conda.sh
+
+# Activate conda env
 conda activate mge_env
 
 
-snakemake \
-   --snakefile Snakefile \
-   --configfile config.yaml \
-   --unlock
 
-snakemake \
-   --snakefile Snakefile \
-   --configfile config.yaml \
-   --cores 16
+
+##Snakemake
+
+# Unlock directory
+snakemake --snakefile ./workflow/Snakefile --configfile ./config/config.yaml --unlock
+
+# Run snakemake workflow on cluster
+snakemake --verbose \
+          --cluster "sbatch --parsable --partition=day --signal=USR2@90 --mem={resources.mem_mb}MB --cpus-per-task={threads} --output=slurm-%j-%x.out --error=slurm-%j-%x.err" \
+          --cluster-config ./config/cluster_config.yaml \
+          --cores 28 \
+          --jobs 15 \
+          --snakefile ./workflow/Snakefile \
+          --configfile ./config/config.yaml \
+          --rerun-incomplete \
+          --latency-wait 60
