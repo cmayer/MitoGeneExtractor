31357aec68dc03c204c82f0958a242e8f3b28698
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2025-04-02 15:16:29 +0100

    Update snakemake.sh

--- snakemake/snakemake.sh
+++ snakemake/snakemake.sh
@@ -3,7 +3,7 @@
 ##These may need some tweaking, although 5-20G of memory and 1-4 CPUs is likely more than enough.
 #SBATCH --job-name=mge
 #SBATCH --partition=long
-#SBATCH --mem=15G
+#SBATCH --mem=8G
 #SBATCH --cpus-per-task=4
 
 
@@ -28,12 +28,12 @@ conda activate mge_env
 snakemake --snakefile ./workflow/Snakefile --configfile ./config/config.yaml --unlock
 
 # Run snakemake workflow on cluster
-snakemake \
-          --cluster "sbatch --parsable --partition=day --signal=USR2@90 --mem={resources.mem_mb}MB --cpus-per-task={threads} --output=slurm-%j-%x.out --error=slurm-%j-%x.err" \
-          --cluster-config ./config/cluster_config.yaml \
-          --cores 28 \
-          --jobs 15 \
-          --snakefile ./workflow/Snakefile \
-          --configfile ./config/config.yaml \
-          --rerun-incomplete \
-          --latency-wait 60
+snakemake --snakefile ./workflow/Snakefile \
+  --configfile ./config/config.yaml \
+  --cluster-config ./config/cluster_config.yaml \
+  --cluster "sbatch --parsable --partition=[cluster.partition} --cpus-per-task={cluster.cpus-per-task} --mem={cluster.mem} --output=slurm-%j-%x.out --error=slurm-%j-%x.err" \
+  --jobs 20 \
+  --cores 32 \
+  --rerun-incomplete \
+  --latency-wait 60
+
